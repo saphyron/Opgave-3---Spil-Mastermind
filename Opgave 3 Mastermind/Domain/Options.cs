be5fd89c0ddbe0f1b51c5ad3307ff106d4de2ae8
John Grandt Markvard Høeg <30288325+saphyron@users.noreply.github.com>
2025-09-24 08:41:13 +0200

    satte tilbage til default options

--- Opgave 3 Mastermind/Domain/Options.cs
+++ Opgave 3 Mastermind/Domain/Options.cs
@@ -3,8 +3,8 @@ using Microsoft.AspNetCore.Mvc.Razor;
 namespace Opgave_3_Mastermind.Domain
 {
     public record Options(
-        int længde = 3,
-        int maxForsøg = 9,
+        int længde = 4,
+        int maxForsøg = 12,
         bool showEmojis = true,
         Sprog sprog = Sprog.Da
     );
